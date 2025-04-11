b0ac2a207bcccf9f8c81e5769f60d998d1473d23
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2025-04-11 12:08:17 -0400

    removed seconds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,6 @@ export default function WeightTracker() {
           day: 'numeric',
           hour: 'numeric',
           minute: '2-digit',
-          second: '2-digit',
           hour12: true
         }).replace(',', ',')
 
